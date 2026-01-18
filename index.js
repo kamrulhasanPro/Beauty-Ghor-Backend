dd156d2d2ca770b809dc4fa1e4f6a96172197891
kamrulmiazi <kamrul116522@gmail.com>
2026-01-18 13:20:55 +0600

    added products api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = 1000;
 const app = express();
 
@@ -51,6 +51,46 @@ app.listen(port, () => {
 
 // ------------products---------------
 
+// get all products
+app.get("/products", async (req, res) => {
+  try {
+    const result = await productsCollection.find().toArray();
+    res.json(result);
+  } catch (error) {
+    console.log("Products find problem", error);
+    res.status(500).json({
+      message: "products find problem",
+    });
+  }
+});
+
+// get a specific product
+app.get("/product/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    const result = await productsCollection.findOne(query);
+    res.json(result);
+  } catch (error) {
+    console.log("Specific products find problem", error);
+    res.status(500).json({
+      message: "Specific product find problem",
+    });
+  }
+});
+
+// post product
+app.post("/products", async (req, res) => {
+  try {
+    const product = req.body;
+    const result = await productsCollection.insertOne(product);
+    res.json(result);
+  } catch (error) {
+    console.log("Product post problem", error);
+    res.status(500).json({
+      message: "Product post problem",
+    });
+  }
+});
 
 // basic
 app.get("/", async (req, res) => {
